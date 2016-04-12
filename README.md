92f1bedf12aece9ac07c7aa90c571c5b9b8fe3bb
Rob Culliton <rob.culliton@gmail.com>
2016-04-12 13:34:20 -0400

    work with command line model for now

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ I want more books like "The Wind Up Bird Chronicles"
 
 1. how to find the title?
 2. how to figure out similar to?
+
+
+UI ideas
+- while returning, display "indiana is typing"
--- speach.py
+++ speach.py
@@ -1,9 +1,20 @@
 import nltk
 
-text = nltk.word_tokenize("I want to borrow a bike")
 
-parts_of_speech = nltk.pos_tag(text)
 
-nouns = [noun for (noun, pos) in parts_of_speech if pos == "NN"]
+user_input = raw_input("You need something fast. Indy can help. : ")
 
-print nouns
+print user_input
+
+## if contains question mark, or contians the word what, explain what this service is
+
+
+
+
+# text = nltk.word_tokenize("I want to borrow a bike")
+#
+# parts_of_speech = nltk.pos_tag(text)
+#
+# nouns = [noun for (noun, pos) in parts_of_speech if pos == "NN"]
+#
+# print nouns
